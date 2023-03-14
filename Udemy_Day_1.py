842ec8134d949dad876a7d17e8a8ec491917ed6e
harigaze <dlrkd1122@gmail.com>
2023-03-14 16:06:40 +0900

    Udemy_Day 1 입출력, 변수 학습

---
+++ Udemy_Day_1.py
@@ -0,0 +1,25 @@
+# 주석 input 함수는 괄호안 내용을 출력하고 입력값을 저장해놓음, 그 입력값을 그대로 출력할 수 있음.
+# 하지만 변수인게 아니므로 다른 행에서 사용은 불가.
+# print("Hello " + input("What is your name?")+ "!")
+
+# 문자열 길이는 len()함수
+# print(len(input("What is your name?")))
+
+# 변수 값 스왑
+
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# # Write your code below this line 👇
+# temp = b
+# b = a
+# a = temp
+# # Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
