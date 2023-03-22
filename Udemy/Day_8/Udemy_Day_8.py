ba32bce8e2c2fea5e26144990d69498cd7c96881
harigaze <dlrkd1122@gmail.com>
2023-03-22 17:10:07 +0900

    Udemy Day 7, 8

---
+++ Udemy/Day_8/Udemy_Day_8.py
@@ -0,0 +1,33 @@
+# # # # function input
+
+# # # def ciper(name):
+# # #     print(name)
+# # #     print("b")
+# # #     print("c")
+
+# # something = 123
+
+# # something = 패러미터 (함수에 전달된 데이터의 이름)
+# # 123 = 아규먼트 (데이터)
+
+# # more than two parameter
+
+# def two(asd, efd):
+#     print(asd)
+#     print(efd)
+# two (asb = 1, efd = 3)
+# two (efd = 2 , asb = 5)
+
+# Write your code below this line 👇
+def paint_calc(height, width, cover):
+    print(f"You'll need {round(height * width / coverage)} cans of paint.")
+
+
+# Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
