fd6bfea0d6597d264d3d18ce427ad86e18481311
harigaze <dlrkd1122@gmail.com>
2023-05-24 17:18:19 +0900

    Udemy api(pixela api, requests(post, put, delete))

--- Udemy/Day_38/main.py
+++ Udemy/Day_38/main.py
@@ -1,4 +1,6 @@
+import os
 import requests
+from datetime import datetime
 
 nut_api_id = "d028dcfa"
 nut_api_key = "52c470f7feabb9963a54edd3f1ea6472"
@@ -17,5 +19,23 @@ nut_body = {
     "age": 24
 }
 
+sheety_url = "https://api.sheety.co/b522a052a88212ddc882983bd34104a6/myWorkouts/workouts"
 response = requests.post(url=nut_api_url, headers=nut_header, json=nut_body)
+workout = response.json()["exercises"][0]
+
+sheety_body = {
+    "workout": {
+        "date" : datetime.now().strftime("%d/%m/%Y"),
+        "time" : datetime.now().strftime("%I:%M:%S %p"),
+        "exercise" : workout["name"],
+        "duration" : workout["duration_min"],
+        "calories" : workout["nf_calories"]
+    }
+}
+sheety_auth = {
+    "Authorization": os.environ.get("sheety_key")
+}
+
+response = requests.post(url= sheety_url, json= sheety_body, headers=sheety_auth)
 print(response.text)
+
