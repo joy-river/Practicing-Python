4b09758e0d16b95463672d5ae2569fe6a0e07107
harigaze <dlrkd1122@gmail.com>
2023-10-30 17:01:21 +0900

    asd

--- Baekjoon/Platinum/PlanetTunnel.py
+++ Baekjoon/Platinum/PlanetTunnel.py
@@ -1,19 +1,17 @@
-class Planet:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
 
-    def cost(self, p):
-        return min(abs(p.x - self.x), abs(p.y - self.y), abs(p.z - self.z))
+from queue import PriorityQueue
 
 
 class UF_tree:
-    def __init__(self, index, planet) -> None:
-        self.index = index
-        self.planet = planet
+    def __init__(self, x, y, z) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
         self.rank = 0
         self.parent = self
+
+    def cost(self, p):
+        return min(abs(p.x - self.x), abs(p.y - self.y), abs(p.z - self.z))
 
     def find(self):
         if self.parent == self:
@@ -29,5 +27,32 @@ class UF_tree:
         if self_root == node_root:
             return
 
+        if self_root.rank < node_root.rank:
+            self_root.parent = node_root
+        else:
+            node_root.parent = self_root
+            if self_root.rank == node_root.rank:
+                self_root.rank += 1
+
 
 n = int(input())
+planets = []
+v = PriorityQueue()
+total_cost = 0
+
+for i in range(n):
+    x, y, z = map(int, input().split())
+    planets.append(UF_tree(x, y, z))
+    for j in range(i):
+        cost = planets[j].cost(planets[i])
+        v.put((cost, (i, j)))
+
+while not v.empty():
+    tunnel = v.get()
+    p1 = planets[tunnel[1][0]]
+    p2 = planets[tunnel[1][1]]
+    if p1.find() != p2.find():
+        p1.union(p2)
+        total_cost += tunnel[0]
+
+print(total_cost)
